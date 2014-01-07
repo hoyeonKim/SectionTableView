27267d75981c47448410657bc1d9fc846f4a7107
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-07 21:50:13 +0900

    SectionTableView
    완료했습니

--- p170/ViewController.m
+++ p170/ViewController.m
@@ -7,17 +7,53 @@
 //
 
 #import "ViewController.h"
+#define CELL_1 @"CELL_1"
+#define CELL_2 @"CELL_2"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource>{
+    NSArray *data1, *data2;
+}
 
 @end
 
 @implementation ViewController
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 2;
+}
 
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if(0==section)
+        return [data1 count];
+    else
+        return [data2 count];
+    }
+
+-(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell;
+    if(0==indexPath.section){
+        cell = [tableView dequeueReusableCellWithIdentifier:CELL_1];
+        cell.textLabel.text = data1[indexPath.row];
+    }
+    else{
+        cell = [tableView dequeueReusableCellWithIdentifier:CELL_2];
+        cell.textLabel.text = data2[indexPath.row];
+    }
+    return cell;
+}
+
+-(NSString *) tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    return [NSString stringWithFormat:@"Section Header %ld",section];
+}
+
+-(NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section{
+    return @"End of Section";
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    data1=@[@"a",@"b",@"c",@"d",@"e"];
+    data2=@[@"1",@"2",@"3",@"4",@"5"];
 }
 
 - (void)didReceiveMemoryWarning
